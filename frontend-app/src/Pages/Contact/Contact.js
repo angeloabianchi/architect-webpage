1f6dc75df12b7ada0e4b02543fd950ba262bae7b
angeloabianchi <angeloabianchi@gmail.com>
2022-02-08 13:23:18 +0100

    contact page updated and added a video background looping at homepage

--- frontend-app/src/Pages/Contact/Contact.js
+++ frontend-app/src/Pages/Contact/Contact.js
@@ -2,17 +2,24 @@ import React from "react";
 import './Contact.css';
 import { Link as ScrollLink } from 'react-scroll';
 import imageScroll from '../../Components/images/goTop.png';
+import logoIcon from '../../Components/images/logoIconFooter.png';
 
 const Contact = () => {
     return(
         <div className="contact-page-container">
             <div className="ContactPageContent" id="contact">
                 <div className="contactInfo">
-                    <h1>CONTACT</h1>
+                    <h1>GET IN TOUCH</h1>
                     <h4>FLAVIA_FLA@ARQUITETA.COM</h4>
                     <h4>CARRER ASTURIAS 9</h4>
                     <h4>08012 BARCELONA</h4>
                     <h4>+34 653 352 120</h4>
+                </div>
+                <div className="contactRights">
+                    <h4>FLAVIA ANDOLFI (C) 2022 ALL RIGHTS RESERVED</h4>
+                </div>
+                <div className="contactLogo">
+                    <ScrollLink to="home" className="logo" smooth={true} duration={1000}><img alt="Logo" src={logoIcon} /></ScrollLink> 
                 </div>
             </div>
 
--- frontend-app/src/Pages/Home/Home.js
+++ frontend-app/src/Pages/Home/Home.js
@@ -2,13 +2,20 @@ import React from "react";
 import './Home.css';
 import { Link as ScrollLink } from 'react-scroll';
 import imageScroll from '../../Components/images/baixa.png';
+import videoHome from '../../Components/images/videoHome.mp4';
 
 const Home = () => {
     return (
         <div className="HomePageContainer">
                 <div className="HomePageContent" id="home">
                     <div className="home">
-                        
+                        <video loop autoPlay className="video">
+                            <source
+                            src={videoHome}
+                            type="video/mp4"
+                            />
+                            Your browser does not support the video tag.
+                        </video>
                     </div>
                 </div>
 
